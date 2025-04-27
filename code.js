cc1defae626a960be9fd3740bb94e73f1686e9ff
Jan Stutz <stutzjan@physik.hu-berlin.de>
2025-04-27 18:14:04 +0200

    try to add a js file to the elemts in case there are only three cards

--- code.js
+++ code.js
@@ -3,7 +3,6 @@ document.querySelectorAll('.card').forEach(card => {
     
     // Überprüfen, ob das .card__img-Element ein Hintergrundbild hat
     if (cardImg && window.getComputedStyle(cardImg).backgroundImage === 'url("")') {
-        // Füge die .no-image-Klasse hinzu, wenn kein Hintergrundbild vorhanden ist
         card.classList.add('no-image');
     }
 });
